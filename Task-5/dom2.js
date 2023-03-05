846657e6ef6969ebdd96a6248d8863f0cd52c323
Balaji Sawant <balaji.swt@gmail.com>
2023-03-05 19:25:32 +0530

    Adding Program files

---
+++ Task-5/dom2.js
@@ -0,0 +1,12 @@
+//GET ELEMENTS BY TAG Names
+// var li = document.getElementsByTagName("li");
+// console.log(li);
+// li[4].style.fontWeight = "bold";
+// li[4].style.color = "red";
+// li[4].textContent = "Balaji";
+
+var li1 = document.getElementsByClassName("item-5");
+console.log(li1);
+li1[0].style.fontWeight = "bold";
+li1[0].style.color = "green";
+li1[0].textContent = "Balaji";
